8b3dba7b9150fe5d0689bb0f1274112519e80114
Nimesh Maharjan <nimesh.maharjan>
2022-07-26 15:22:40 +0545

    starting front-end

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import productRoute from "./routes/productRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 import userRoute from "./routes/userRoute.js";
 import errorMiddleware from "./middleware/error.js";
 import cookieParser from "cookie-parser";
+import auth from "./middleware/checkAuth.js";
 const app = express();
 
 app.use(express.json());
@@ -12,6 +14,7 @@ const URL = "/api/v1";
 
 app.use(`${URL}/products`, productRoute);
 app.use(`${URL}/users`, userRoute);
+app.use(`${URL}/orders`, auth.isUserAuthenticated, orderRoute);
 app.use(errorMiddleware);
 
 export default app;
