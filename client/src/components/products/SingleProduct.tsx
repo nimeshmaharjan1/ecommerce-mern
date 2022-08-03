4ea099173b2f0ad72370ec5013d2083d32098c88
Nimesh Maharjan <nimesh.maharjan>
2022-08-03 13:27:26 +0545

    carousel

--- client/src/components/products/SingleProduct.tsx
+++ client/src/components/products/SingleProduct.tsx
@@ -1,4 +1,4 @@
-import { Col, Row, Button } from "antd";
+import { Col, Row, Button, Carousel } from "antd";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { selectSingleProduct } from "../../store/products/productsSlice";
@@ -9,23 +9,49 @@ const SingleProduct = () => {
   const product: any = useSelector((state: RootState) =>
     selectSingleProduct(state, params.id)
   );
-
+  const contentStyle: React.CSSProperties = {
+    height: "650px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
   return (
     <Row className="row" gutter={24}>
       <Col xs={24} xl={12}>
         <div className="left-column">
-          <img
-            data-image="blue"
-            className="active"
-            src="https://unblast.com/wp-content/uploads/2019/10/Model-T-shirt-Mockup-2.jpg"
-            alt=""
-          />
+          <Carousel effect="fade">
+            <div>
+              <img
+                style={contentStyle}
+                data-image="blue"
+                className="active"
+                src="https://unblast.com/wp-content/uploads/2019/10/Model-T-shirt-Mockup-2.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                style={contentStyle}
+                data-image="blue"
+                className="active"
+                src="https://unblast.com/wp-content/uploads/2019/10/Model-T-shirt-Mockup-2.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <h3 style={contentStyle}>3</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>4</h3>
+            </div>
+          </Carousel>
         </div>
       </Col>
       <Col xl={12} xs={24}>
         <div className="right-column">
           <div className="product-description">
-            <span>{product.category}</span>
+            <span>{product.description}</span>
             <h1>{product.name}</h1>
             <p>{product.description}</p>
           </div>
